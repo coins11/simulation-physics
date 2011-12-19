bbcd82c4e61055ee6e9bcf4e8d5946006808393a
mayth <chimeaquas@hotmail.com>
2011-12-20 06:19:58 +0900

    Add SimulationPhys3_6 into solution. Implements SimulationPhys3_6.

--- mayth/SimulationPhys3_6/Program.cs
+++ mayth/SimulationPhys3_6/Program.cs
@@ -10,6 +10,41 @@ namespace SimulationPhys3_6
     {
         static void Main(string[] args)
         {
+            int[][] matrix = new int[5][];
+            foreach (var x in Enumerable.Range(0, 5))
+                matrix[x] = Enumerable.Repeat(0, 5).ToArray();
+
+            // calc by round
+            foreach (var p in Utilities.GetRandomPointEnumerator(563, 0, 4).Take(200))
+                matrix[(int)Math.Round(p.X)][(int)Math.Round(p.Y)]++;
+
+            // calc by distance
+            //foreach (var p in Utilities.GetRandomPointEnumerator(563, 0, 4).Take(200))
+            //{
+            //    double min = Math.Pow(p.X, 2) + Math.Pow(p.Y, 2);
+            //    var minPoint = Tuple.Create(0, 0);
+            //    foreach (var y in Enumerable.Range(0, 5))
+            //    {
+            //        foreach (var x in Enumerable.Range(0, 5))
+            //        {
+            //            double dist = Math.Pow(p.X - x, 2) + Math.Pow(p.Y - y, 2);
+            //            if (dist < min)
+            //            {
+            //                min = dist;
+            //                minPoint = Tuple.Create(x, y);
+            //            }
+            //        }
+            //    }
+            //    matrix[minPoint.Item1][minPoint.Item2]++;
+            //}
+
+            foreach (var row in matrix)
+            {
+                foreach (var v in row)
+                    Console.Write(v + "\t");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
