36e5706b66e41cf37451c44643b53692e6a0ebcc
mayth <chimeaquas@hotmail.com>
2011-12-20 04:43:37 +0900

    Add a document comment for Utilities.cs

--- mayth/SimulationPhysLib/Utilities.cs
+++ mayth/SimulationPhysLib/Utilities.cs
@@ -10,6 +10,16 @@ namespace SimulationPhysLib
     /// </summary>
     public static class Utilities
     {
+        /// <summary>
+        /// Enumerates <see cref="Point"/> structures infinitely.
+        /// </summary>
+        /// <param name="seed">A seed number to initialize <see cref="Random"/> class.</param>
+        /// <param name="min">The inclusive lower bound of the random number returned.</param>
+        /// <param name="max">The inclusive upper bound of the random number returned. <paramref name="max"/> must be greater than or equal to <paramref name="min"/>.</param>
+        /// <returns>
+        /// An enumerator of <see cref="Point"/> structure.
+        /// These <see cref="Point"/> structures have double values greater than or equal to <paramref name="min"/> and less than or equal to <paramref name="max"/>. If <paramref name="min"/> equals <paramref name="max"/>, <paramref name="min"/> is returned.
+        /// </returns>
         public static IEnumerable<Point> GetRandomPointEnumerator(int seed, int min, int max)
         {
             var rand = new Random(seed);
