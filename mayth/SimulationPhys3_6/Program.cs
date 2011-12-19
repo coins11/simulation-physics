d828f3b2ce58893b63e6ddffc71c3136a30cc479
mayth <chimeaquas@hotmail.com>
2011-12-20 04:50:26 +0900

    Create a new project for 3rd week, number 6.

---
+++ mayth/SimulationPhys3_6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimulationPhysLib;
+
+namespace SimulationPhys3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
