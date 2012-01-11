5314b862893038e6981b3e2f4b70ed32d616ae28
mayth <chimeaquas@hotmail.com>
2012-01-12 03:32:04 +0900

    Add IEnumerableExtension.

---
+++ mayth/SimulationPhysLib/IEnumerableExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimulationPhysLib
+{
+    public static class IEnumerableExtension
+    {
+        public static IEnumerable<TResult> Map<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> func)
+        {
+            foreach (var element in source)
+                yield return func(element);
+        }
+    }
+}
